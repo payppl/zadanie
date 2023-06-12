ac03d9c2af290b2b0dcadc5b525303461a07ca79
Paypl <paypl2001@gmail.com>
2023-06-12 15:25:25 +0200

    Initial commit

---
+++ php/zadanie2.php
@@ -0,0 +1,34 @@
+<?php
+
+class TextInput {
+
+    protected $value = "";
+    
+    public function add($text) {
+        $this->value .= $text;
+    }
+
+    public function getValue() {
+        return $this->value;
+    }
+}
+
+class NumericInput extends TextInput {
+    public function add($text) {
+        if(ctype_digit($text)) {
+            parent::add($text);
+        }
+    }
+}
+
+$textInput = new TextInput();
+$textInput->add("Witam ");
+$textInput->add("was\n");
+echo $textInput->getValue();
+
+$numericInput = new NumericInput();
+$numericInput->add("5353");
+$numericInput->add("fghdfh");
+$numericInput->add("1111");
+echo $numericInput->getValue(); 
+?>
